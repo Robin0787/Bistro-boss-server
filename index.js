548631843bd18d37301270482453c03c764c0388
Robin <mohammadrobin636@gmail.com>
2023-05-31 02:47:53 +0600

    Added user add and remove routes

--- index.js
+++ index.js
@@ -25,9 +25,45 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("All-Foods").collection("users");
     const itemsCollection = client.db("All-Foods").collection("items");
     const reviewsCollection = client.db("All-Foods").collection("reviews");
     const cartCollection = client.db("All-Foods").collection("cart");
+
+    // Users Related Api
+
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async(req, res) => {
+        const user = req.body;
+        const query = {email: user.email};
+        const checkUser = await userCollection.findOne(query);
+        if(checkUser){
+          return res.send({message: 'User Exits in database'});
+        }
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role: "admin"
+        }
+      }
+      const result = await userCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    })
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // create json web token for the user
     app.post("/get-token", async (req, res) => {
@@ -71,7 +107,7 @@ async function run() {
     // Deleting specific Item
     app.delete('/delete/:id', async(req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = {_id: id};
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
